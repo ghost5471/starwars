02dfd706b5373680fb5bf0b1199ad6daae817def
ghost5471 <vclaudinogomes@gmail.com>
2024-10-05 02:21:54 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-import axios from 'axios';
-//const axios = require('axios'); // legacy way
+//const personagensContador = document.getElementById("personagens").innerHTML = 22;
+//const navesContador = document.getElementById("naves").innerHTML = 22;
+//const planetasContador = document.getElementById("planetas").innerHTML = 22;
+//const luassContador = document.getElementById("luas").innerHTML = 22;
+//
+//function preencherContadores(){
+//  personagensContador.innerHTML = }
 
-// Make a request for a user with a given ID
-axios.get('https://swapi.dev/api/people/1/')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+//function swapiGet(){}
+
+const axios = require('axios')
+
+const url = 'https://swapi.dev/api/people/'
+
+async function getRepos() {
+  let response = await axios.get(url)
+  console.log(response.data)
+}
+
+getRepos()
